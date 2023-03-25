d44d728d26773c1f8003c6d6951f9fbed15a2660
Wan Sim <0420syj@naver.com>
2023-03-26 04:11:25 +0900

    [feature] Import NewButton

--- apps/mariage/src/pages/counter.tsx
+++ apps/mariage/src/pages/counter.tsx
@@ -1,9 +1,17 @@
 import Counter from "../components/counter";
+import { NewButton } from "@vinami/ui";
 
 function CounterPage() {
     return (
         <div>
             <h1>Counter Page</h1>
+            <NewButton
+                label="Button"
+                onClick={() => {
+                    console.log("clicked");
+                }}
+                primary
+            />
             <Counter />
         </div>
     );
--- common/ui/src/NewButton/NewButton.stories.tsx
+++ common/ui/src/NewButton/NewButton.stories.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { ComponentStory, ComponentMeta } from "@storybook/react";
 
-import { NewButton } from "./NewButton";
+import { NewButton } from ".";
 
 // More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
 export default {
