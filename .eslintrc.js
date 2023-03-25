8643ef0338c6a7c79235505f9a2c24145103321d
Wan Sim <0420syj@naver.com>
2023-03-26 01:10:16 +0900

    [Feature] Add Next.js Eslint configuration
    https://nextjs.org/docs/basic-features/eslint#migrating-existing-config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,13 @@
-export default {
+module.exports = {
     "env": {
         "browser": true,
-        "es2021": true
+        "es2021": true,
+        "node": true
     },
     "extends": [
         "eslint:recommended",
-        "plugin:@typescript-eslint/recommended"
+        "plugin:@typescript-eslint/recommended",
+        "plugin:@next/next/recommended"
     ],
     "overrides": [
     ],
