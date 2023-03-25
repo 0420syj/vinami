6fc1e72015a8f53f906f1a2710aee91b22a2bac8
Wan Sim <0420syj@naver.com>
2023-03-26 01:32:40 +0900

    [feature] Make simple counter
    just for fun :)

---
+++ apps/mariage/src/pages/counter.tsx
@@ -0,0 +1,12 @@
+import Counter from "../components/counter";
+
+function CounterPage() {
+    return (
+        <div>
+            <h1>Counter Page</h1>
+            <Counter />
+        </div>
+    );
+}
+
+export default CounterPage;
