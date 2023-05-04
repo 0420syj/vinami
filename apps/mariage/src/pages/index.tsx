ae84c98c56e66d1e789d8461522d41752e10fbd8
Wan Sim <0420syj@naver.com>
2023-05-04 23:23:46 +0900

    [feature] Add button

--- apps/mariage/src/pages/index.tsx
+++ apps/mariage/src/pages/index.tsx
@@ -4,6 +4,7 @@ import MetaHead from "@/components/MetaHead";
 import FoodSelect from "@/components/FoodSelect";
 import Footer from "@/components/Footer";
 import Header from "@/components/Header";
+import { Button } from "@vinami/ui";
 
 const Home: NextPage = () => {
     const [selectedFood, setSelectedFood] = useState("");
@@ -15,16 +16,25 @@ const Home: NextPage = () => {
             .then(data => setFoodOptions(data));
     }, []);
 
+    const handleButtonClick = () => {
+        console.log("Button clicked!");
+    };
+
     return (
         <>
             <MetaHead />
             <Header />
             <main>
-                <FoodSelect
-                    options={foodOptions}
-                    selectedFood={selectedFood}
-                    onSelectFood={food => setSelectedFood(food)}
-                />
+                <div
+                    style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center" }}
+                >
+                    <FoodSelect
+                        options={foodOptions}
+                        selectedFood={selectedFood}
+                        onSelectFood={food => setSelectedFood(food)}
+                    />
+                    <Button backgroundColor="#5F021F" label="Click here" onClick={handleButtonClick} primary />
+                </div>
             </main>
             <Footer />
         </>
