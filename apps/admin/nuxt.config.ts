e9b40bf247771b7e0ecd8e2a915ff563834e27b9
Wan Sim <0420syj@naver.com>
2023-05-11 17:41:45 +0900

    [feature] Add Tailwind CSS in Admin

--- apps/admin/nuxt.config.ts
+++ apps/admin/nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
     // transpile: ["@mariage/ui"],
+    css: ["~/assets/css/main.css"],
+    postcss: {
+        plugins: {
+            tailwindcss: {},
+            autoprefixer: {},
+        },
+    },
 });
